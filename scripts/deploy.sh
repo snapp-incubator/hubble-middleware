484e3f388d26ee30ae70d0eee9756f7a841b16c7
saman.hoseini <saman.hoseini@snapp.cab>
2022-12-09 22:54:45 +0000

    fix: correct image repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,1 @@
-sudo docker build -t registry.okd4.teh-1.snappcloud.io/public/hubble-middleware:$1 .
-sudo docker push registry.okd4.teh-1.snappcloud.io/public/hubble-middleware:$1
+sudo docker build -t quay.io/snapp/hubble-middleware:$1 --push .
